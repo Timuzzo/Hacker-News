b1df20fed602ee730bcd159f66c63c89205d97f4
Anton <avonpoblozki@posteo.de>
2023-04-17 15:38:40 +0200

    ADD four main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
+import NewsItem from "./components/NewsItem";
+import NewsList from "./components/NewsList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      hello world
     </div>
   );
 }
